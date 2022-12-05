2dd001984685c161d85713bf80025a1b29e5282d
ArsenKoch <arsenkocaran@icloud.com>
2022-12-06 00:58:59 +0300

    Create a data class for the View

---
+++ app/src/main/java/com/example/customview2/TicTacToeField.kt
@@ -0,0 +1,33 @@
+package com.example.customview2
+
+
+enum class Cell {
+    PLAYER_1,
+    PLAYER_2,
+    EMPTY
+}
+
+typealias OnFieldChangedListener = (field: TicTacToeField) -> Unit
+
+class TicTacToeField(
+    private val rows: Int,
+    private val columns: Int
+) {
+
+    private val cells = Array(rows) { Array(columns) { Cell.EMPTY } }
+
+    private val listeners = mutableSetOf<OnFieldChangedListener>()
+
+    fun getCell(rows: Int, columns: Int): Cell {
+        if (rows < 0 || columns < 0 || rows >= rows || columns >= columns) return Cell.EMPTY
+        return cells[rows][columns]
+    }
+
+    fun setCell(rows: Int, columns: Int, cell: Cell) {
+        if (rows < 0 || columns < 0 || rows >= rows || columns >= columns) return
+        if (getCell(rows, columns) != cell) {
+            cells[rows][columns]
+            listeners.forEach { it.invoke(this) }
+        }
+    }
+}
