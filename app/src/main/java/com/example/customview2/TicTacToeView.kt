f50b7ba7f23f983b4430d2a798f29bd0139aef60
ArsenKoch <arsenkocaran@icloud.com>
2022-12-07 21:57:51 +0300

    Set the data to display in the View

--- app/src/main/java/com/example/customview2/TicTacToeView.kt
+++ app/src/main/java/com/example/customview2/TicTacToeView.kt
@@ -17,25 +17,49 @@ class TicTacToeView(
     private var playerColor2 by Delegates.notNull<Int>()
     private var gridColor by Delegates.notNull<Int>()
 
-    constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) : this(context,attributeSet, defStyleAttr, R.style.DefaultTicTacToeFieldStyle  )
-    constructor(context: Context, attributeSet: AttributeSet?) : this(context, attributeSet, R.attr.TicTacToeFieldStyle)
+    var ticTacToeField: TicTacToeField? = null
+        set(value) {
+            field = value
+            requestLayout()
+            invalidate()
+        }
+
+    constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) : this(
+        context,
+        attributeSet,
+        defStyleAttr,
+        R.style.DefaultTicTacToeFieldStyle
+    )
+
+    constructor(context: Context, attributeSet: AttributeSet?) : this(
+        context,
+        attributeSet,
+        R.attr.TicTacToeFieldStyle
+    )
+
     constructor(context: Context) : this(context, null)
 
     init {
-        if(attributeSet != null){
+        if (attributeSet != null) {
             initAttributes(attributeSet, defStyleAttr, defStyleRes)
         } else {
             initDefaultColors()
         }
     }
 
-    private fun initAttributes(attributeSet: AttributeSet?,defStyleAttr: Int,defStyleRes: Int){
-        val typedArray = context.obtainStyledAttributes(attributeSet,R.styleable.TicTacToeView,
-           defStyleAttr, defStyleRes)
-        playerColor1 = typedArray.getColor(R.styleable.TicTacToeView_playerColor1,
-            PLAYER1_DEFAULT_COLOR)
-        playerColor2 = typedArray.getColor(R.styleable.TicTacToeView_playerColor2,
-            PLAYER2_DEFAULT_COLOR)
+    private fun initAttributes(attributeSet: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) {
+        val typedArray = context.obtainStyledAttributes(
+            attributeSet, R.styleable.TicTacToeView,
+            defStyleAttr, defStyleRes
+        )
+        playerColor1 = typedArray.getColor(
+            R.styleable.TicTacToeView_playerColor1,
+            PLAYER1_DEFAULT_COLOR
+        )
+        playerColor2 = typedArray.getColor(
+            R.styleable.TicTacToeView_playerColor2,
+            PLAYER2_DEFAULT_COLOR
+        )
         gridColor = typedArray.getColor(R.styleable.TicTacToeView_gridColor, GRID_DEFAULT_COLOR)
 
         typedArray.recycle()
